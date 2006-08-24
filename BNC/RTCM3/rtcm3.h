7c49ea6124a1ff5743fd6d55f3525d7ba6161807
mervart <mervart@a76a543e-5a0c-11df-b9b2-fd73e79bcebd>
2006-08-24 13:24:14 +0000

    *** empty log message ***

--- BNC/RTCM3/rtcm3.h
+++ BNC/RTCM3/rtcm3.h
@@ -1,2 +1,16 @@
 
-// Initially an empty file
+#ifndef RTCM3_H
+#define RTCM3_H
+
+#include "../RTCM/GPSDecoder.h"
+#include "cgps_transform.h"
+
+class rtcm3 : public GPSDecoder {
+public:
+  rtcm3();
+  ~rtcm3();
+  void Decode(char* buffer = 0, int bufLen = 0);
+private:
+} ;
+
+#endif
