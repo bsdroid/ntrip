d267687d99176d4190eaf77d38c4298ac97aac1f
mervart <mervart@a76a543e-5a0c-11df-b9b2-fd73e79bcebd>
2006-08-27 08:45:36 +0000

    *** empty log message ***

--- trunk/BNC/bncrinex.cpp
+++ trunk/BNC/bncrinex.cpp
@@ -26,12 +26,27 @@ using namespace std;
 // Constructor
 ////////////////////////////////////////////////////////////////////////////
 bncRinex::bncRinex(const char* StatID) {
-  _statID = StatID;
+  _statID        = StatID;
+  _headerWritten = false;
 }
 
 // Destructor
 ////////////////////////////////////////////////////////////////////////////
 bncRinex::~bncRinex() {
+  _out.close();
+}
+
+// Write RINEX Header
+////////////////////////////////////////////////////////////////////////////
+void bncRinex::writeHeader() {
+
+  // Open the Output File
+  // --------------------
+  QByteArray fname = _statID + ".RXO";
+  _out.open(fname.data());
+
+
+  _headerWritten = true;
 }
 
 // Stores Observation into Internal Array
@@ -52,15 +67,21 @@ void bncRinex::dumpEpoch() {
     return;
   }
 
+  // Write RINEX Header
+  // ------------------
+  if (!_headerWritten) {
+    writeHeader();
+  }
+
   // Time of Epoch
   // -------------
   struct converttimeinfo cti;
   Observation* firstObs = *_obs.begin();
   converttime(&cti, firstObs->GPSWeek, firstObs->GPSWeeks);
 
-  cout.setf(std::ios::fixed);
+  _out.setf(std::ios::fixed);
 
-  cout << setw(3)  << cti.year%100
+  _out << setw(3)  << cti.year%100
        << setw(3)  << cti.month
        << setw(3)  << cti.day
        << setw(3)  << cti.hour
@@ -73,26 +94,30 @@ void bncRinex::dumpEpoch() {
   while (it.hasNext()) {
     iSat++;
     Observation* ob = it.next();
-    cout << " " << setw(2) << int(ob->SVPRN);
+    _out << " " << setw(2) << int(ob->SVPRN);
     if (iSat == 12 && it.hasNext()) {
-      cout << endl << "                                ";
+      _out << endl << "                                ";
       iSat = 0;
     }
   }
-  cout << endl;
-
-  cout.precision(3);
+  _out << endl;
 
   it.toFront();
   while (it.hasNext()) {
     Observation* ob = it.next();
-    cout << setw(14) << ob->C1
-         << setw(14) << ob->P2
-         << setw(14) << ob->L1
-         << setw(14) << ob->L2 << endl;
+
+    char lli = ' ';
+    char snr = ' ';
+    _out << setw(14) << setprecision(3) << ob->C1 << lli << snr;
+    _out << setw(14) << setprecision(3) << ob->P2 << lli << snr; 
+    _out << setw(14) << setprecision(3) << ob->L1 << lli << snr; 
+    _out << setw(14) << setprecision(3) << ob->L2 << lli << snr; 
+    _out << endl;
+
     delete ob;
   }
 
+  _out.flush();
   _obs.clear();
 }
 
