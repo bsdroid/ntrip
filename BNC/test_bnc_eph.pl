59b832ae7b99d86ebb725928afcf3cd506f7b015
weber <weber@a76a543e-5a0c-11df-b9b2-fd73e79bcebd>
2007-12-18 16:15:01 +0000

    *** empty log message ***

---
+++ BNC/test_bnc_eph.pl
@@ -0,0 +1,33 @@
+#!/usr/local/bin/perl -w
+
+use strict;
+use IO::Socket;
+
+# List of Parameters
+# ------------------
+my($port) = @ARGV;
+
+if (!defined($port)) {
+  die "Usage: test_bnc_eph.pl portNumber\n";
+}
+
+# Local Variables
+# ---------------
+my($serverHostName) = "localhost";
+my $server;
+
+my $retries = 10;
+while ($retries--) {
+  $server = IO::Socket::INET->new( Proto    => "tcp",
+                                   PeerAddr => $serverHostName,
+                                   PeerPort => $port);
+  last if ($server);
+}
+die "Cannot connect to $serverHostName on $port: $!" unless ($server);
+
+my $buffer;
+while (defined ($buffer = <$server>)) {
+  print $buffer;
+}
+
+
