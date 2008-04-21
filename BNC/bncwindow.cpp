da474ff04cc6b14fda31821d93229e6348201d94
weber <weber@a76a543e-5a0c-11df-b9b2-fd73e79bcebd>
2008-04-21 14:10:10 +0000

    *** empty log message ***

--- BNC/bncwindow.cpp
+++ BNC/bncwindow.cpp
@@ -61,7 +61,7 @@ bncWindow::bncWindow() {
 
   setMinimumSize(77*ww, 65*ww);
 
-  setWindowTitle(tr("BKG Ntrip Client (BNC) Version 1.5"));
+  setWindowTitle(tr("BKG Ntrip Client (BNC) Version 1.6"));
 
   // Create Actions
   // --------------
@@ -783,7 +783,7 @@ bncAboutDlg::bncAboutDlg(QWidget* parent) :
   QLabel* img = new QLabel();
   img->setPixmap(QPixmap(":ntrip-logo.png"));
   dlgLayout->addWidget(img, 0,0);
-  dlgLayout->addWidget(new QLabel("BKG NTRIP Client (BNC) Version 1.5"), 0,1);
+  dlgLayout->addWidget(new QLabel("BKG NTRIP Client (BNC) Version 1.6"), 0,1);
   dlgLayout->addWidget(tb,1,0,1,2);
   dlgLayout->addWidget(_closeButton,2,1,Qt::AlignRight);  
 
