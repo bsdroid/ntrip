5d4013a1b86b77c0b2775a9da840c069b8c0bacc
mervart <mervart@a76a543e-5a0c-11df-b9b2-fd73e79bcebd>
2008-04-25 09:44:33 +0000

    *** empty log message ***

--- BNS/bnssp3.h
+++ BNS/bnssp3.h
@@ -7,11 +7,29 @@
 
 class bnsSP3 {
  public:
-   bnsSP3();
-   ~bnsSP3();
-   void write(int GPSweek, double GPSweeks, const QString& prn, 
-              const ColumnVector& xx);
+  bnsSP3();
+  ~bnsSP3();
+  void write(int GPSweek, double GPSweeks, const QString& prn, 
+             const ColumnVector& xx);
+
+  static QString nextEpochStr(const QDateTime& datTim,
+                              const QString& intStr, 
+                              QDateTime* nextEpoch = 0);
+
  private:
+  void resolveFileName(const QDateTime& datTim);
+  void writeHeader(const QDateTime& datTim);
+  void closeFile();
+
+  int           _samplingRate;
+  bool          _headerWritten;
+  QDateTime     _nextCloseEpoch;
+  std::ofstream _out;
+  QString       _path;
+  QString       _intr;
+  QString       _ext;
+  QString       _ID4;
+  QByteArray    _fName;
 };
 
 #endif
