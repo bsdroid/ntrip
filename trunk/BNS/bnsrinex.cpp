8f649fb0be1bafb8bca70c5a868a5a94657e8811
mervart <mervart@a76a543e-5a0c-11df-b9b2-fd73e79bcebd>
2008-04-25 14:07:11 +0000

    *** empty log message ***

--- trunk/BNS/bnsrinex.cpp
+++ trunk/BNS/bnsrinex.cpp
@@ -15,6 +15,7 @@
  *
  * -----------------------------------------------------------------------*/
 
+#include <math.h>
 #include <iomanip>
 
 #include "bnsrinex.h"
@@ -36,7 +37,7 @@ bnsRinex::~bnsRinex() {
 // Write Header
 ////////////////////////////////////////////////////////////////////////////
 void bnsRinex::writeHeader(const QDateTime& datTim) {
-  _out << "THIS IS A DUMMY HEADER" << endl;
+  _out << "THIS IS A DUMMY CLOCK RINEX HEADER" << endl;
 }
 
 // Write One Epoch
@@ -45,6 +46,15 @@ t_irc bnsRinex::write(int GPSweek, double GPSweeks, const QString& prn,
                    const ColumnVector& xx) {
 
   if (bnsoutf::write(GPSweek, GPSweeks, prn, xx) == success) {
+
+      QDateTime datTim = dateAndTimeFromGPSweek(GPSweek, GPSweeks);
+      double sec = fmod(GPSweeks, 60.0);
+    
+      _out << "AS " << prn.toAscii().data()
+           << datTim.toString("  yyyy MM dd hh mm").toAscii().data()
+           << setw(12) << setprecision(8) << sec << "  2  "
+           << setw(20) << setprecision(12) << xx(4) 
+           << setw(20) << setprecision(12) << 0.0 << endl;
 
     return success;
   }
