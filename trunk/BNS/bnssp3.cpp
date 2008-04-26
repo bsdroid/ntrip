2ff946e3522564ff192d92426d8b0d99f9df5692
mervart <mervart@a76a543e-5a0c-11df-b9b2-fd73e79bcebd>
2008-04-26 08:55:42 +0000

    *** empty log message ***

--- trunk/BNS/bnssp3.cpp
+++ trunk/BNS/bnssp3.cpp
@@ -38,12 +38,6 @@ bnsSP3::bnsSP3(const QString& prep, const QString& ext, const QString& path,
 bnsSP3::~bnsSP3() {
 }
 
-// Write Header
-////////////////////////////////////////////////////////////////////////////
-void bnsSP3::writeHeader(const QDateTime& datTim) {
-  _out << "THIS IS A DUMMY SP3 HEADER" << endl;
-}
-
 // Write One Epoch
 ////////////////////////////////////////////////////////////////////////////
 t_irc bnsSP3::write(int GPSweek, double GPSweeks, const QString& prn, 
@@ -73,3 +67,39 @@ t_irc bnsSP3::write(int GPSweek, double GPSweeks, const QString& prn,
     return failure;
   }
 }
+
+// Close File (write last line)
+////////////////////////////////////////////////////////////////////////////
+void bnsSP3::closeFile() {
+  _out << "EOF" << endl;
+  bnsoutf::closeFile();
+}
+
+// Write Header
+////////////////////////////////////////////////////////////////////////////
+void bnsSP3::writeHeader(const QDateTime& datTim) {
+  _out << "#cP2007  7  1  0  0  0.00000000      96 ORBIT IGS05 HLM  IGS"
+       << "## 1434      0.00000000   900.00000000 54282 0.0000000000000"
+
+  _out << "+   32   G01G02G03G04G05G06G07G08G09G10G11G12G13G14G15G16G17\n"
+       << "+        G18G19G20G21G22G23G24G25G26G27G28G29G30G31G32  0  0\n"
+       << "+          0  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0\n"
+       << "+          0  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0\n"
+       << "+          0  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0\n"
+       << "++         0  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0\n"
+       << "++         0  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0\n"
+       << "++         0  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0\n"
+       << "++         0  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0\n"
+       << "++         0  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0\n"
+       << "%c cc cc ccc ccc cccc cccc cccc cccc ccccc ccccc ccccc ccccc\n"
+       << "%c cc cc ccc ccc cccc cccc cccc cccc ccccc ccccc ccccc ccccc\n"
+       << "%f  0.0000000  0.000000000  0.00000000000  0.000000000000000\n"
+       << "%f  0.0000000  0.000000000  0.00000000000  0.000000000000000\n"
+       << "%i    0    0    0    0      0      0      0      0         0\n"
+       << "%i    0    0    0    0      0      0      0      0         0\n"
+       << "/*                                                          \n"
+       << "/*                                                          \n"
+       << "/*                                                          \n"
+       << "/*                                                          \n";
+}
+
