87a710b99d936387e9e98d1c83b9c9f77ba990b6
mervart <mervart@a76a543e-5a0c-11df-b9b2-fd73e79bcebd>
2008-05-06 08:00:16 +0000

    *** empty log message ***

--- BNS/bns.h
+++ BNS/bns.h
@@ -53,8 +53,9 @@ class t_bns : public QThread {
   void deleteBnsEph();
   void openCaster();
   void readEpoch();
-  void processSatellite(int GPSweek, double GPSweeks, const QString& prn, 
-                        const ColumnVector& xx, struct ClockOrbit::SatData* sd);
+  void processSatellite(gpsEph* ep, int GPSweek, double GPSweeks, 
+                        const QString& prn, const ColumnVector& xx, 
+                        struct ClockOrbit::SatData* sd);
 
   QTcpServer*               _clkServer;
   QTcpSocket*               _clkSocket;
