e5aea4a0585c0e5f57bf6c71d676c398e2d0acaf
mervart <mervart@a76a543e-5a0c-11df-b9b2-fd73e79bcebd>
2008-05-09 06:34:06 +0000

    *** empty log message ***

--- BNC/bncapp.cpp
+++ BNC/bncapp.cpp
@@ -197,11 +197,11 @@ void bncApp::slotNewGlonassEph(glonassephemeris* glonasseph) {
   if (*ee != 0) {
     wwOld  = (*ee)->GPSWeek;
     towOld = (*ee)->GPSTOW; 
-    updatetime(&wwOld, &towOld, (*ee)->tb*1000, 1);
+    updatetime(&wwOld, &towOld, (*ee)->tb*1000, 0);
 
     wwNew  = glonasseph->GPSWeek;
     towNew = glonasseph->GPSTOW; 
-    updatetime(&wwNew, &towNew, glonasseph->tb*1000, 1);
+    updatetime(&wwNew, &towNew, glonasseph->tb*1000, 0);
   }
 
   if ( *ee == 0      || 
@@ -481,7 +481,7 @@ void bncApp::printGlonassEph(glonassephemeris* ep, bool printFile) {
   int tow = ep->GPSTOW; 
   struct converttimeinfo cti;
 
-  updatetime(&ww, &tow, ep->tb*1000, 1);
+  updatetime(&ww, &tow, ep->tb*1000, 0);
   converttime(&cti, ww, tow);
 
   int tk = ep->tk-3*60*60; 
