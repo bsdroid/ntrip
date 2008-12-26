7f2ece7fba2fe1954d5ad368dba07e77ed9872f2
mervart <mervart@a76a543e-5a0c-11df-b9b2-fd73e79bcebd>
2008-12-26 10:22:44 +0000

    *** empty log message ***

--- BNC/ntrip2test.cpp
+++ BNC/ntrip2test.cpp
@@ -12,10 +12,13 @@ int main(int argc, char *argv[]) {
   QApplication app(argc, argv, false);
 
 
-  bncNetRequest* req = new bncNetRequest();
+  bncNetRequest* req1 = new bncNetRequest();
+  QUrl url1("http://euref-ip.bkg.bund.de:2111/");
+  req1->request(url1);
 
-  QUrl url("http://ntrip2c:rtcm2c@euref-ip.bkg.bund.de:2111");
-  req->request(url);
+  bncNetRequest* req2 = new bncNetRequest();
+  QUrl url2("http://ntrip2c:rtcm2c@euref-ip.bkg.bund.de:2111/TEST1");
+  req2->request(url2);
 
   return app.exec();
 }
