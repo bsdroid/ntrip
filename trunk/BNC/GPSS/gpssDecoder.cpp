84f63cb856a0ea1bcf8251e6819712f2ba421ddc
mervart <mervart@a76a543e-5a0c-11df-b9b2-fd73e79bcebd>
2009-01-03 11:53:57 +0000

    *** empty log message ***

--- trunk/BNC/GPSS/gpssDecoder.cpp
+++ trunk/BNC/GPSS/gpssDecoder.cpp
@@ -38,6 +38,27 @@ typedef struct epochHeader {
   int    n_svs;
 } EPOCHHEADER;
 
+// Cyclic Redundancy Check
+////////////////////////////////////////////////////////////////////////////
+unsigned short cal_crc(char *buf, int num) {
+  unsigned short polynomial = 0x8408;
+  unsigned short crc = 0;
+  int i;
+  while ( num-- ) {
+    crc = ( crc & 0xFF00 ) | ( *buf++^( crc & 0x00FF ) );
+    for( i=0; i<8; i++ ){
+      if( crc & 0x0001 ){
+        crc >>= 1;
+        crc ^= polynomial;
+      }
+      else{
+        crc >>= 1;
+      }
+    }
+  }
+  return (crc);
+}
+
 // Constructor
 ////////////////////////////////////////////////////////////////////////////
 gpssDecoder::gpssDecoder() : GPSDecoder() {
@@ -60,6 +81,8 @@ t_irc gpssDecoder::Decode(char* data, int dataLen, vector<string>& errmsg) {
 
   _buffer += QByteArray(data, dataLen);
 
+  cout << "Decode: buffer length = " << _buffer.length() << endl;
+
   int iBeg;
   while ( (iBeg = _buffer.indexOf(0x02)) != -1) {
     _buffer = _buffer.mid(iBeg);
@@ -73,8 +96,6 @@ t_irc gpssDecoder::Decode(char* data, int dataLen, vector<string>& errmsg) {
 
       int reqLength = 2 + sizeof(recordSize) + sizeof(EPOCHHEADER);
 
-      cout << "Obs: " << _buffer.length() << " " << reqLength << endl;
-
       if (_buffer.length() >= reqLength) {
         EPOCHHEADER epochHdr;
         memcpy(&epochHdr, _buffer.data() + 2 + sizeof(recordSize), 
@@ -83,6 +104,14 @@ t_irc gpssDecoder::Decode(char* data, int dataLen, vector<string>& errmsg) {
         reqLength += epochHdr.n_svs * sizeof(t_obsInternal) + sizeof(crc) + 1;
 
         if (_buffer.length() >= reqLength) {
+
+          int checkLen = 2 + sizeof(recordSize) + sizeof(EPOCHHEADER) + 
+                         epochHdr.n_svs * sizeof(t_obsInternal);
+          memcpy(&crc, _buffer.data() + checkLen, sizeof(crc));
+          int crdCal = cal_crc(_buffer.data(), checkLen);
+
+          cout << "Obs: " << crc << " " << crdCal << endl;
+
           for (int is = 0; is < epochHdr.n_svs; is++) {
             t_obs* obs = new t_obs();
             memcpy(&(obs->_o), _buffer.data() + 2 + sizeof(recordSize) + 
@@ -101,9 +130,14 @@ t_irc gpssDecoder::Decode(char* data, int dataLen, vector<string>& errmsg) {
       int reqLength = 2 + sizeof(recordSize) + sizeof(gpsephemeris) +
         sizeof(crc) + 1;
 
-      cout << "Eph: " << _buffer.length() << " " << reqLength << endl;
-
       if (_buffer.length() >= reqLength) {
+
+        int checkLen = 2 + sizeof(recordSize) + sizeof(gpsephemeris);
+        memcpy(&crc, _buffer.data() + checkLen, sizeof(crc));
+        int crdCal = cal_crc(_buffer.data(), checkLen);
+
+        cout << "Obs: " << crc << " " << crdCal << endl;
+
         gpsephemeris* gpsEph = new gpsephemeris;
         memcpy(gpsEph, _buffer.data() + 2 + sizeof(recordSize), 
                sizeof(gpsephemeris));
