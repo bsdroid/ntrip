f416d6c741636f4893c392b631523a64479cac68
mervart <mervart@a76a543e-5a0c-11df-b9b2-fd73e79bcebd>
2009-01-03 09:54:32 +0000

    *** empty log message ***

--- BNC/GPSS/gpssDecoder.cpp
+++ BNC/GPSS/gpssDecoder.cpp
@@ -1,4 +1,4 @@
-
+\
 /* -------------------------------------------------------------------------
  * BKG NTRIP Client
  * -------------------------------------------------------------------------
@@ -56,91 +56,54 @@ t_irc gpssDecoder::Decode(char* data, int dataLen, vector<string>& errmsg) {
 
   errmsg.clear();
 
-  if (_mode == MODE_SEARCH) {
-    _buffer.clear();
-  }
-  _buffer.append(data, dataLen);
+  _buffer += QByteArray(data, dataLen);
 
-  for (;;) { 
+  int iBeg;
+  while ( (iBeg = _buffer.indexOf(0x02)) != -1) {
+    _buffer = _buffer.mid(iBeg);
 
-    if (_buffer.size() < 1) {
-      _mode = MODE_SEARCH;
-      return success;
+    // Record Size
+    // -----------
+    int recordSize;
+    if (_buffer.length() >= int(2 + sizeof(recordSize)) &&
+        ( char(_buffer[1]) == 0x00 || char(_buffer[1]) == 0x01) ) {
+      memcpy(&recordSize, _buffer.data()+2, sizeof(recordSize)); 
     }
-
-    if      (_mode == MODE_SEARCH) {
-      if (_buffer[0] == 0x02) {
-        _mode = MODE_TYPE;
-      }
-      else {
-        _mode = MODE_SEARCH;
-      }
-      _buffer.erase(0,1);
+    else {
+      break;
     }
-
-    else if (_mode == MODE_TYPE) {
-      if        (_buffer[0] == 0x00) {
-        _mode = MODE_EPOCH;
-      } else if (_buffer[0] == 0x01) {
-        _mode = MODE_EPH;
-      } else {
-        _mode = MODE_SEARCH;
-      }
-      _buffer.erase(0,1);
-    }
-
-    else if (_mode == MODE_EPOCH || _mode == MODE_EPH) {
-      int recordSize;
-      if (_buffer.size() < sizeof(recordSize)) {
-        _mode = MODE_SEARCH;
-        return success;
-      }
-      memcpy(&recordSize, _buffer.data(), sizeof(recordSize)); 
-      if (_mode == MODE_EPOCH) {
-        _mode = MODE_EPOCH_BODY;
-      }
-      if (_mode == MODE_EPH) {
-        _mode = MODE_EPH_BODY;
-      }
-      _buffer.erase(0,sizeof(recordSize));
-    }
-
-    else if (_mode == MODE_EPOCH_BODY) {
+   
+    // Observations
+    // ------------
+    if      (char(_buffer[1]) == 0x00) {
       EPOCHHEADER epochHdr;
-      if (_buffer.size() < sizeof(epochHdr)) {
-        _mode = MODE_SEARCH;
-        return success;    
+      if (_buffer.length() >= int(2 + sizeof(recordSize) + sizeof(epochHdr))) {
+        memcpy(&epochHdr, _buffer.data() + 2 + sizeof(recordSize), 
+               sizeof(epochHdr));
+
+        if (_buffer.length() >= int(2 + sizeof(recordSize) + sizeof(epochHdr) +
+                                epochHdr.n_svs * sizeof(t_obsInternal))) {
+          for (int is = 1; is <= epochHdr.n_svs; is++) {
+            t_obs* obs = new t_obs();
+            memcpy(&(obs->_o), _buffer.data(), sizeof(t_obsInternal));
+            _obsList.push_back(obs);
+          }
+        }
       }
-      memcpy(&epochHdr, _buffer.data(), sizeof(epochHdr));
-      _buffer.erase(0,sizeof(epochHdr));
-      if (_buffer.size() < epochHdr.n_svs * sizeof(t_obsInternal)) {
-        _mode = MODE_SEARCH;
-        return success;
-      }
-      for (int is = 1; is <= epochHdr.n_svs; is++) {
-        t_obs* obs = new t_obs();
-        memcpy(&(obs->_o), _buffer.data(), sizeof(t_obsInternal));
-        _obsList.push_back(obs);
-        _buffer.erase(0, sizeof(t_obsInternal));
-      }
-      _mode = MODE_EPOCH_CRC;
     }
 
-    else if (_mode == MODE_EPH_BODY) {
-      if (_buffer.size() < sizeof(gpsephemeris)) {
-        _mode = MODE_SEARCH;
-        return success;
+    // Ephemeris
+    // ---------
+    else if (char(_buffer[1]) == 0x01) {
+      if (_buffer.length() >= int(2+sizeof(recordSize)+ sizeof(gpsephemeris))){
+        gpsephemeris* gpsEph = new gpsephemeris;
+        memcpy(gpsEph, _buffer.data(), sizeof(gpsephemeris));
+        emit newGPSEph(gpsEph);
       }
-      gpsephemeris* gpsEph = new gpsephemeris;
-      memcpy(gpsEph, _buffer.data(), sizeof(gpsephemeris));
-      emit newGPSEph(gpsEph);
-      _buffer.erase(0, sizeof(gpsephemeris));
-      _mode = MODE_EPH_CRC;
     }
 
     else {
-      _buffer.erase(0,1);
-      _mode = MODE_SEARCH;
+      _buffer == _buffer.mid(1);
     }
   }
 
