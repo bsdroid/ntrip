463e5173a7b8913c293dbee0cb17cb8d1bc4b046
weber <weber@a76a543e-5a0c-11df-b9b2-fd73e79bcebd>
2009-02-19 12:41:34 +0000

    *** empty log message ***

--- trunk/BNC/bncnetqueryv0.cpp
+++ trunk/BNC/bncnetqueryv0.cpp
@@ -4,7 +4,7 @@
  *
  * Class:      bncNetQueryV0
  *
- * Purpose:    Blocking Network Requests (NTRIP Version 1)
+ * Purpose:    Direct TCP/IP Network Requests no NTRIP
  *
  * Author:     L. Mervart
  *
@@ -105,39 +105,6 @@ void bncNetQueryV0::startRequest(const QUrl& url, const QByteArray& gga) {
     return;
   }
 
-  // Send Request
-  // ------------
-  QString uName = QUrl::fromPercentEncoding(_url.userName().toAscii());
-  QString passW = QUrl::fromPercentEncoding(_url.password().toAscii());
-  QByteArray userAndPwd;
-
-  if(!uName.isEmpty() || !passW.isEmpty()) {
-    userAndPwd = "Authorization: Basic " + (uName.toAscii() + ":" +
-    passW.toAscii()).toBase64() + "\r\n";
-  }
-
-  QByteArray reqStr;
-  if (_url.path().indexOf("/") != 0) _url.setPath("/");
-  reqStr = "GET " + _url.path().toAscii() + " HTTP/1.0\r\n"
-           + "User-Agent: NTRIP BNC/" BNCVERSION "\r\n"
-           + userAndPwd + "\r\n";
-
-  // NMEA string to handle VRS stream
-  // --------------------------------
-  if (!gga.isEmpty()) {
-    reqStr += gga + "\r\n";
-  }
-
-  _socket->write(reqStr, reqStr.length());
-
-  if (!_socket->waitForBytesWritten(_timeOut)) {
-    delete _socket;
-    _socket = 0;
-    _status = error;
-    emit newMessage(_url.path().toAscii() + " write timeout", true);
-    return;
-  }
-
   // Read Caster Response
   // --------------------
   QStringList response;
@@ -152,21 +119,9 @@ void bncNetQueryV0::startRequest(const QUrl& url, const QByteArray& gga) {
     if (_socket->canReadLine()) {
       QString line = _socket->readLine();
       response.push_back(line);
-      if (line.trimmed().isEmpty()) {
-        break;
-      }
-      if (line.indexOf("200 OK") == -1) { // != weber
-        response.clear();
-        break;
-      }
+      response.clear();
+      break;
     }
-  }
-  if (response.size() > 0) {
-    delete _socket;
-    _socket = 0;
-    _status = error;
-    emit newMessage(_url.path().toAscii() + " wrong caster response\n" +
-                    response.join("\n").toAscii(), true);
   }
 }
 
