4d6b531bbd3006e700f94a7542c404af02999137
weber <weber@a76a543e-5a0c-11df-b9b2-fd73e79bcebd>
2009-02-24 14:18:00 +0000

    *** empty log message ***

--- trunk/BNC/bncnetqueryv0.cpp
+++ trunk/BNC/bncnetqueryv0.cpp
@@ -102,6 +102,8 @@ void bncNetQueryV0::startRequest(const QUrl& url, const QByteArray& gga) {
     delete _socket; 
     _socket = 0;
     _status = error;
+      emit(newMessage(_url.path().toAscii().replace(0,1,"")
+                      + ": Connect timeout, reconnecting", true));
     return;
   }
 
@@ -113,7 +115,8 @@ void bncNetQueryV0::startRequest(const QUrl& url, const QByteArray& gga) {
       delete _socket;
       _socket = 0;
       _status = error;
-      emit newMessage(_url.path().toAscii() + " read timeout", true);
+      emit newMessage(_url.path().toAscii().replace(0,1,"")
+                      + ": Read timeout", true);
       return;
     }
     if (_socket->canReadLine()) {
