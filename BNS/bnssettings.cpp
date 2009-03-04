b917a5a8f9b0c0a6771b37295366d79e907145e2
weber <weber@a76a543e-5a0c-11df-b9b2-fd73e79bcebd>
2009-03-04 08:34:15 +0000

    *** empty log message ***

--- BNS/bnssettings.cpp
+++ BNS/bnssettings.cpp
@@ -26,24 +26,27 @@ bnsSettings::bnsSettings() :
     setValue("proxyHost",   "");
     setValue("proxyPort",   "");
     setValue("logFile",     "");
+    setValue("autoStart",   "0");
     setValue("fileAppend",  "0");
-    setValue("refSys_1",    "IGS05");
-    setValue("refSys_2",    "IGS05");
-    setValue("inpEcho",     "");
     setValue("ephHost",     "");
     setValue("ephPort",     "");
     setValue("ephEcho",     "");
     setValue("clkPort",     "");
+    setValue("inpEcho",     "");
     setValue("outHost1",    "");
     setValue("outPort1",    "");
+    setValue("mountpoint_1","");
+    setValue("password1",   "");
+    setValue("refSys_1",    "IGS05");
+    setValue("outFile_1",   "");
+    setValue("beClocks1",   "0");
     setValue("outHost2",    "");
     setValue("outPort2",    "");
-    setValue("mountpoint_1","");
     setValue("mountpoint_2","");
-    setValue("outFile_1",   "");
-    setValue("outFile_2",   "");
-    setValue("password1",   "");
     setValue("password2",   "");
+    setValue("refSys_2",    "IGS05");
+    setValue("outFile_2",   "");
+    setValue("beClocks2",   "0");
     setValue("rnxPath",     "");
     setValue("rnxIntr",     "1 min");
     setValue("rnxSampl",    "0");
@@ -51,9 +54,6 @@ bnsSettings::bnsSettings() :
     setValue("sp3Intr",     "10 min");
     setValue("sp3Sampl",    "0");
     setValue("startTab",    "0");
-    setValue("autoStart",   "0");
-    setValue("beClocks1",   "0");
-    setValue("beClocks2",   "0");
     sync();
   }
 }
