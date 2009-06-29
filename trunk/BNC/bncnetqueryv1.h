163e8882942854e23f22ccb940d0f1307c208ffe
mervart <mervart@a76a543e-5a0c-11df-b9b2-fd73e79bcebd>
2009-06-29 09:26:45 +0000

    *** empty log message ***

--- trunk/BNC/bncnetqueryv1.h
+++ trunk/BNC/bncnetqueryv1.h
@@ -4,8 +4,6 @@
 #include "bncnetquery.h"
 
 class bncNetQueryV1 : public bncNetQuery {
- Q_OBJECT
-
  public:
   bncNetQueryV1();
   virtual ~bncNetQueryV1();
@@ -15,15 +13,11 @@ class bncNetQueryV1 : public bncNetQuery {
   virtual void startRequest(const QUrl& url, const QByteArray& gga);
   virtual void waitForReadyRead(QByteArray& outData);
 
- private slots:
-  void slotFinished();
-
  private:
   void startRequestPrivate(const QUrl& url, const QByteArray& gga, 
                            bool sendRequestOnly);
   QEventLoop* _eventLoop;
   QTcpSocket* _socket;
-  QByteArray  _outData;
 };
 
 #endif
