7d4e24e5198f2855d2f4da9250c81d1470ceedb2
weber <weber@a76a543e-5a0c-11df-b9b2-fd73e79bcebd>
2009-11-17 15:23:39 +0000

    *** empty log message ***

--- BNC/bncfigurelate.h
+++ BNC/bncfigurelate.h
@@ -34,7 +34,7 @@ class bncFigureLate : public QWidget {
   ~bncFigureLate();
   void updateMountPoints();
  public slots:
-  void slotNewData(const QByteArray staID, double clate);
+  void slotNewLatency(const QByteArray staID, double clate);
  protected:
   void paintEvent(QPaintEvent *event);
  private slots:
