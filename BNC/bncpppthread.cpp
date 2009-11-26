f2db2d2a1be39be614d1b2b3ef73aaaf749854df
mervart <mervart@a76a543e-5a0c-11df-b9b2-fd73e79bcebd>
2009-11-26 10:53:49 +0000

    *** empty log message ***

--- BNC/bncpppthread.cpp
+++ BNC/bncpppthread.cpp
@@ -55,6 +55,7 @@ bncPPPthread::bncPPPthread(QByteArray staID) {
   _staID         = staID;
   _isToBeDeleted = false;
   _data          = 0;
+  _dataHlp       = 0;
 }
 
 // Destructor
@@ -64,6 +65,7 @@ bncPPPthread::~bncPPPthread() {
     wait();
   }
   delete _data;
+  delete _dataHlp;
   QMapIterator<QString, t_eph*> it(_eph);
   while (it.hasNext()) {
     it.next();
@@ -114,30 +116,32 @@ void bncPPPthread::putNewObs(p_obs pp) {
 
   t_time tt(obs->GPSWeek, obs->GPSWeeks);
 
-  QString prn = 
+  if      (!_dataHlp) {
+    _dataHlp = new t_data();
+    _dataHlp->tt = tt;
+  }
+  else if (tt != _dataHlp->tt) {
+    _data = _dataHlp;
+    _dataHlp = new t_data();
+    _dataHlp->tt = tt;
+  }
+
+  ++_dataHlp->numSat;
+
+  if (_dataHlp->numSat > t_data::MAXOBS) {
+    cerr << "putNewObs: numSat > MAXOBS\n";
+    exit(1);
+  }
+
+  _dataHlp->prn[_dataHlp->numSat] = 
         QString("%1%2").arg(obs->satSys).arg(obs->satNum, 2, 10, QChar('0'));
-
-  cout << tt.timestr(1) << " " << prn.toAscii().data() << endl;
-  cout.flush();
-
-///  delete _data;
-///  _data = new t_data();
-///
-///    QByteArray staID = QByteArray(obs->StatID); 
-///    if (staID == _staID) {
-///      if (_data->tt.undef()) {
-///      }
-///      ++_data->numSat;
-///      _data->prn[_data->numSat] = 
-///          
-///      _data->C1[_data->numSat] = obs->C1;
-///      _data->C2[_data->numSat] = obs->C2;
-///      _data->P1[_data->numSat] = obs->P1;
-///      _data->P2[_data->numSat] = obs->P2;
-///      _data->L1[_data->numSat] = obs->L1;
-///      _data->L2[_data->numSat] = obs->L2;
-///    }
-///  }
+      
+  _dataHlp->C1[_dataHlp->numSat] = obs->C1;
+  _dataHlp->C2[_dataHlp->numSat] = obs->C2;
+  _dataHlp->P1[_dataHlp->numSat] = obs->P1;
+  _dataHlp->P2[_dataHlp->numSat] = obs->P2;
+  _dataHlp->L1[_dataHlp->numSat] = obs->L1;
+  _dataHlp->L2[_dataHlp->numSat] = obs->L2;
 }
 
 // 
