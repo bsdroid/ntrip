7dc5287a7305935b6010c124a50329b4e518702d
mervart <mervart@a76a543e-5a0c-11df-b9b2-fd73e79bcebd>
2010-01-12 21:59:34 +0000

    *** empty log message ***

--- BNC/bncpppclient.cpp
+++ BNC/bncpppclient.cpp
@@ -336,10 +336,12 @@ t_irc bncPPPclient::getSatPos(const bncTime& tt, const QString& prn,
     ColumnVector xcTst(4);
     ColumnVector vvTst(3);
     bncTime ttTst(tt.gpsw(), floor(tt.gpssec()+0.5));
-    ee->position(ttTst.gpsw(), ttTst.gpssec(), xcTst.data(), vvTst.data());
-    cout.setf(ios::fixed);
-    cout << "A: " << ttTst.timestr() << " " << prn.toAscii().data() << " "
-         << xcTst.t();
+    if (prn[0] == 'R') {
+      ee->position(ttTst.gpsw(), ttTst.gpssec(), xcTst.data(), vvTst.data());
+      cout.setf(ios::fixed);
+      cout << "A: " << ttTst.timestr() << " " << prn.toAscii().data() << " "
+           << xcTst.t();
+    }
     //// end test
 
     if (CORR_REQUIRED) {
@@ -347,10 +349,12 @@ t_irc bncPPPclient::getSatPos(const bncTime& tt, const QString& prn,
         t_corr* cc = _corr.value(prn);
 
         //// beg test
-        applyCorr(cc, xcTst, vvTst);
-        cout << "B: " << ttTst.timestr() << " " 
-             << ee->IOD() << "  " <<  cc->iod << "  " << (tt - cc->tt) << " "
-             << prn.toAscii().data() << " " << xcTst.t();
+        if (prn[0] == 'R') {
+          applyCorr(cc, xcTst, vvTst);
+          cout << "B: " << ttTst.timestr() << " " 
+               << ee->IOD() << "  " <<  cc->iod << "  " << (tt - cc->tt) << " "
+               << prn.toAscii().data() << " " << xcTst.t();
+        }
         //// beg test
 
         if (ee->IOD() == cc->iod && (tt - cc->tt) < MAXAGE) {
