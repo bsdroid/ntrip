f7329f95d5441ab77b6a4ca6cdde93bb28ea9284
weber <weber@a76a543e-5a0c-11df-b9b2-fd73e79bcebd>
2012-05-22 13:44:24 +0000

    RINEX navigation edit, concatenation and quality check options blocked

--- trunk/BNC/bncwindow.cpp
+++ trunk/BNC/bncwindow.cpp
@@ -2323,9 +2323,9 @@ void bncWindow::slotBncTextChanged(){
     bool enable10 = _reqcActionComboBox->currentText() == "Edit/Concatenate";
     enableWidget(enable &&  enable10, _reqcEditOptionButton);
     enableWidget(enable,              _reqcObsFileChooser);
-    enableWidget(enable,              _reqcNavFileChooser);
+    enableWidget(false ,              _reqcNavFileChooser); // TODO
     enableWidget(enable &&  enable10, _reqcOutObsLineEdit);
-    enableWidget(enable &&  enable10, _reqcOutNavLineEdit);
+    enableWidget(false  &&  enable10, _reqcOutNavLineEdit); // TODO
     enableWidget(enable && !enable10, _reqcOutLogLineEdit);
   }
 
