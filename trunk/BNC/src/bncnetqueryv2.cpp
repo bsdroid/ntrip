aac31e2d77c82e5ddaf08919cb664a39fd736525
stuerze <stuerze@a76a543e-5a0c-11df-b9b2-fd73e79bcebd>
2015-12-08 14:54:49 +0000

    minor changes

--- trunk/BNC/src/bncnetqueryv2.cpp
+++ trunk/BNC/src/bncnetqueryv2.cpp
@@ -152,6 +152,9 @@ void bncNetQueryV2::startRequestPrivate(const QUrl& url, const QByteArray& gga,
   }
   request.setRawHeader("Connection"   , "close");
 
+  if (_reply) {
+    delete _reply;
+  }
   _reply = _manager->get(request);
 
   // Connect Signals
