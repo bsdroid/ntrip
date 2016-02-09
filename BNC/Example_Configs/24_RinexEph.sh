2b2e39c09f255a95713cb95a524dbbbe3bdcc9ec
weber <weber@a76a543e-5a0c-11df-b9b2-fd73e79bcebd>
2016-02-09 20:14:40 +0000

    Documentation completed

--- BNC/Example_Configs/24_RinexEph.sh
+++ BNC/Example_Configs/24_RinexEph.sh
@@ -4,8 +4,8 @@
        -key mountPoints "//Example:Configs@products.igs-ip.net:80/RTCM3EPH RTCM_3 DEU 50.09 8.66 no 1" \
        -key ephPath Output \
        -key logFile Output/RinexEph.log \
-       -key ephIntr 1 hour \
-       -key ephV3 2
+       -key ephIntr "1 hour" \
+       -key ephV3 2 &
 
 psID=`echo $!`
 sleep 20
