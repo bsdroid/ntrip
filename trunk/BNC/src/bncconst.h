38a0b912c06f773e15b2c0f87f918308210d1a27
stuerze <stuerze@a76a543e-5a0c-11df-b9b2-fd73e79bcebd>
2018-07-06 09:00:20 +0000

    updated RINEX obs codes

--- trunk/BNC/src/bncconst.h
+++ trunk/BNC/src/bncconst.h
@@ -31,71 +31,110 @@ enum t_irc {failure = -1, success, fatal}; // return code
 
 class t_frequency {
  public:
-  enum type {dummy = 0, G1, G2, G5, R1, R2,
+  enum type {dummy = 0,
+                        // GPS
+                        G1,
+                        G2,
+                        G5,
+                        // GLONASS
+                        R1, // G1
+ //                       R1a,// G1a / 1600.995
+                        R2, // G2
+//                        R2a,// G1b / 1248.06
+                        R3, // G3  / 1202.025
+                        // Galileo
                         E1, // E1  / 1575.42
                         E5, // E5a / 1176.45
                         E7, // E5b / 1207.140
                         E8, // E5(E5a+E5b) / 1191.795
                         E6, // E6  / 1278.75
+                        // QZSS
                         J1, // 1575.42
                         J2, // 1227.60
                         J5, // 1176.45
-                        J6, // Lex(6) 1278.75
-                        S1, // 1575.42
-                        S5, // 1176.45
+                        J6, // 1278.75
+                        // BDS
                         C2, // 1561.098
+                        C1, // 1575.42
+                        C5, // 1176.45
                         C7, // 1207.14
                         C6, // 1268.52
+                        // IRNSS
                         I5, // 1176.45
                         I9, // 2492.028
+                        // SBAS
+                        S1, // 1575.42
+                        S5, // 1176.45
              max};
 
   static std::string toString(type tt) {
+    // GPS
     if      (tt == G1) return "G1";
     else if (tt == G2) return "G2";
     else if (tt == G5) return "G5";
+    // GLONASS
     else if (tt == R1) return "R1";
+//    else if (tt == R1a) return "R5";
     else if (tt == R2) return "R2";
+//    else if (tt == R2a) return "R7";
+    else if (tt == R3) return "R3";
+    // Galileo
     else if (tt == E1) return "E1";
     else if (tt == E5) return "E5";
     else if (tt == E6) return "E6";
     else if (tt == E7) return "E7";
     else if (tt == E8) return "E8";
+    // QZSS
     else if (tt == J1) return "J1";
     else if (tt == J2) return "J2";
     else if (tt == J5) return "J5";
     else if (tt == J6) return "J6";
-    else if (tt == S1) return "S1";
-    else if (tt == S5) return "S5";
+    // BDS
     else if (tt == C2) return "C2";
+    else if (tt == C1) return "C1";
+    else if (tt == C5) return "C5";
     else if (tt == C7) return "C7";
     else if (tt == C6) return "C6";
+    // IRNSS
     else if (tt == I5) return "I5";
     else if (tt == I9) return "I9";
+    // SBAS
+    else if (tt == S1) return "S1";
+    else if (tt == S5) return "S5";
     return std::string();
   }
   static enum type toInt(std::string s) {
+    // GPS
     if      (s == "G1") return G1;
     else if (s == "G2") return G2;
     else if (s == "G5") return G5;
+    // GLONASS
     else if (s == "R1") return R1;
     else if (s == "R2") return R2;
+    else if (s == "R3") return R3;
+    // Galileo
     else if (s == "E1") return E1;
     else if (s == "E5") return E5;
     else if (s == "E6") return E6;
     else if (s == "E7") return E7;
     else if (s == "E8") return E8;
+    // QZSS
     else if (s == "J1") return J1;
     else if (s == "J2") return J2;
     else if (s == "J5") return J5;
     else if (s == "J6") return J6;
-    else if (s == "S1") return S1;
-    else if (s == "S5") return S5;
+    // BDS
     else if (s == "C2") return C2;
+    else if (s == "C1") return C1;
+    else if (s == "C5") return C5;
     else if (s == "C7") return C7;
     else if (s == "C6") return C6;
+    // IRNSS
     else if (s == "I5") return I5;
     else if (s == "I9") return I9;
+    // SBAS
+    else if (s == "S1") return S1;
+    else if (s == "S5") return S5;
     return type();
   }
 };
